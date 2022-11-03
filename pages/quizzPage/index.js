863a3712a7f493f7185b67b4cabce7005444ab73
Arthur Saito <arthur_saito1998@hotmail.com>
2022-11-03 00:02:36 -0300

    Display of form responses is working. No CSS yet. No responsiveness.

--- pages/quizzPage/index.js
+++ pages/quizzPage/index.js
@@ -1,39 +1,35 @@
 import {getQuizzesFromServer} from "../../scripts/api.js";
 let quizzData = await getQuizzesFromServer();
-//console.log(quizzData);
+let quizzQuestion = [];
+console.log(quizzData);
 
 function renderQuizz(){
     const quizzImage = document.getElementById('quizzImage');
     const quizzTitle = document.getElementById('quizzTitle');
     const questionContainer = document.getElementById('questionContainer');
-
     quizzImage.innerHTML +=`<img src="${quizzData[0].image}">`
     quizzTitle.innerHTML +=`<h3>${quizzData[0].title}</h3>`
 
     for(let i = 0; i < quizzData[0].questions.length; i++){
-        const quizzQuestion = quizzData[0].questions[i];
-  
+        quizzQuestion = quizzData[0].questions[i];
         questionContainer.innerHTML += `
         <header class="questionTitle">${quizzQuestion.title}</header>
         <ul class="answersContainer"></ul>`;
-        const answersContainer = document.querySelectorAll('.answersContainer');
-        
+    }
+    
+    let answersContainer = document.querySelectorAll('.answersContainer');
 
-        for (let j = 0; j < quizzData[0].questions[i].answers.length; j++){
-            const quizzAnswer = quizzData[0].questions[i].answers[j];
-            console.log(answersContainer);
-            answersContainer.forEach(element => {
-                console.log(element);
-                element.innerHTML = '';
-                element.innerHTML += ` 
+    for (let i = 0; i < quizzQuestion.answers.length; i++){
+        const quizzAnswer = quizzQuestion.answers[i];
+        answersContainer.forEach(element => {
+            console.log(element);
+            element.innerHTML += ` 
                 <li id="answerImageContainer">
                     <img src="${quizzAnswer.image}" id="answerImage">
                     <p>${quizzAnswer.text}</p>
                 </li>
                 `
-            });
-        }
-
+     });
     }
 }
 
