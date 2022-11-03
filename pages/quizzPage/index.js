f5a6c9d93369b7103571d0bf2ab1e964d2726a6d
Arthur Saito <arthur_saito1998@hotmail.com>
2022-11-03 19:52:19 -0300

    Added CSS on QuestionScreen

--- pages/quizzPage/index.js
+++ pages/quizzPage/index.js
@@ -1,28 +1,37 @@
+/*Get quizzes do servidor e criação de variáveis */
 import {getQuizzesFromServer} from "../../scripts/api.js";
 let quizzData = await getQuizzesFromServer();
-let quizzQuestion = [];
-console.log(quizzData);
+let quizzQuestion = []; 
+let quizzAnswer = [];
 
+
+/*Função principal - Objetivo: Renderizar as propriedades dos objetos dentro do array de quizzes*/
 function renderQuizz(){
     const quizzImage = document.getElementById('quizzImage');
     const quizzTitle = document.getElementById('quizzTitle');
     const questionContainer = document.getElementById('questionContainer');
+    
+
+    /*Renderizar imagem e título do quizz - cabeçalho */
     quizzImage.innerHTML +=`<img src="${quizzData[0].image}">`
     quizzTitle.innerHTML +=`<h3>${quizzData[0].title}</h3>`
 
+    /*Renderizar título das perguntas*/
     for(let i = 0; i < quizzData[0].questions.length; i++){
         quizzQuestion = quizzData[0].questions[i];
         questionContainer.innerHTML += `
         <header class="questionTitle">${quizzQuestion.title}</header>
         <ul class="answersContainer"></ul>`;
     }
-    
-    let answersContainer = document.querySelectorAll('.answersContainer');
+    /*Isso precisa ficar aqui,  haja vista que a criação da classe answersContainer só existe junto à renderização do título das perguntas*/
+    const answersContainer = document.querySelectorAll('.answersContainer');
 
+    console.log(quizzQuestion);
+
+    /*Renderizar as imagens e textos das respostas*/
     for (let i = 0; i < quizzQuestion.answers.length; i++){
-        const quizzAnswer = quizzQuestion.answers[i];
+        quizzAnswer = quizzQuestion.answers[i];
         answersContainer.forEach(element => {
-            console.log(element);
             element.innerHTML += ` 
                 <li id="answerImageContainer">
                     <img src="${quizzAnswer.image}" id="answerImage">
@@ -31,6 +40,13 @@ function renderQuizz(){
                 `
      });
     }
+    
 }
+
+/* for( let i = 0; i < quizzQuestion.answers.length; i++){
+    let j = Math.floor(Math.random() * (i +1))
+    [quizzAnswer[i],quizzAnswer[j]] = [quizzAnswer[j],quizzAnswer[i]]
+} */
+
 
 renderQuizz();
